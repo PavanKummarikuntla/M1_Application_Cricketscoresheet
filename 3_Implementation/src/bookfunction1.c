2aea4e06d641dc08f6f133f09ca59497227ba2ae
Pavan Kumar Kummarikuntla <94158653+PavanKummarikuntla@users.noreply.github.com>
2021-11-24 11:06:04 +0530

    Update bookfunction1.c

--- 3_Implementation/src/bookfunction1.c
+++ 3_Implementation/src/bookfunction1.c
@@ -1,13 +1,3 @@
-/**
- * @file bookfunction1.c
- * @author Pavan
- * @brief 
- * @version 0.1
- * @date 2021-09-06
- * 
- * @copyright Copyright (c) 2021
- * 
- */
 #include<stdio.h>
 #include<string.h>
 #include<time.h>
