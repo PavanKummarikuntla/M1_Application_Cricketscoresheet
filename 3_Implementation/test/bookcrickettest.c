52f0d15b347cbd8b11196f57e1a25ec80a9f2e19
Pavan Kumar Kummarikuntla <94158653+PavanKummarikuntla@users.noreply.github.com>
2021-11-24 11:07:23 +0530

    Update bookcrickettest.c

--- 3_Implementation/test/bookcrickettest.c
+++ 3_Implementation/test/bookcrickettest.c
@@ -1,14 +1,3 @@
-/**
- * @file bookcrickettest.c
- * @author Pavan
- * @brief 
- * @version 0.1
- * @date 2021-09-07
- * 
- * @copyright Copyright (c) 2021
- * 
- */
-
 #include "unity.h"
 
 /* Modify these two lines according to the project */
