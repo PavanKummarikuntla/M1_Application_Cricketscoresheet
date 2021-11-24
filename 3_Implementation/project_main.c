320b83fe6f4003abeef07cd6354e2174f22d8962
Pavan Kumar Kummarikuntla <94158653+PavanKummarikuntla@users.noreply.github.com>
2021-11-24 11:13:12 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -1,13 +1,3 @@
-/**
- * @file main1.c
- * @author Pavan
- * @brief 
- * @version 0.1
- * @date 2021-09-07
- * 
- * @copyright Copyright (c) 2021
- * 
- */
 #include"stdio.h"
 #include"string.h"
 #include"time.h"
